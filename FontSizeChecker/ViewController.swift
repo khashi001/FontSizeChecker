e472655aa43c2ea8fc9e5e588a6f8d00465b3f0f
khashi001 <taurus@kokoroyutakani.com>
2017-08-07 11:09:17 +0900

    とりあえず動いたのでひとまず完成！

--- FontSizeChecker/ViewController.swift
+++ FontSizeChecker/ViewController.swift
@@ -9,10 +9,31 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var frameWidthText: UITextField!
+    
+    @IBOutlet weak var frameWidthLabel: UILabel!
+    
+    @IBOutlet weak var frameHeightText: UITextField!
+    
+    @IBOutlet weak var frameHeightLabel: UILabel!
+    
+    
+    @IBOutlet weak var fontSizeTextField: UITextField!
+    
+    
+    @IBOutlet weak var fontSizeLabel: UILabel!
+    
+    
+    @IBOutlet weak var pointTextField: UITextField!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +41,38 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    
+    
+    @IBAction func frameWidthText(_ sender: UITextField) {
+        let senderString = sender.text!
+        let senderCGFloat : CGFloat = CGFloat((senderString as NSString).doubleValue)
+        pointTextField.frame.size.width = senderCGFloat
+    }
+    
+    @IBAction func frameHeightText(_ sender: UITextField) {
+        let senderString = sender.text!
+        let senderCGFloat : CGFloat = CGFloat((senderString as NSString).doubleValue)
+        pointTextField.frame.size.height = senderCGFloat
+    }
+    
+    
+    @IBAction func fontSizeTextField(_ sender: UITextField) {
+        let senderString = sender.text!
+        let senderCGFloat : CGFloat = CGFloat((senderString as NSString).doubleValue)
+        pointTextField.font = UIFont.systemFont(ofSize: senderCGFloat)
+    }
+    
+    
+    @IBAction func pointTextField(_ sender: Any) {
+    }
+    
+    
+    
+    
+    
+    
+    
+    
 
 }
 
