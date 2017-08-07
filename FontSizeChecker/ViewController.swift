091b5879806307c5c2cc3ca73641bc13610ddfa2
khashi001 <taurus@kokoroyutakani.com>
2017-08-07 09:42:24 +0900

    Initial Commit

---
+++ FontSizeChecker/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FontSizeChecker
+//
+//  Created by 橋本久美子 on 2017/08/07.
+//  Copyright © 2017年 橋本久美子. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
